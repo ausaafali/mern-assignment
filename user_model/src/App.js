a553f8814c6e639d2bf087d8431e7572674a3f9d
asp <asp@aptech.com>
2025-04-24 11:00:28 +0500

    for assignments

--- user_model/src/App.js
+++ user_model/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import OwlCarousel from 'react-owl-carousel';
+// import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Register from './Components/Register';
--- user_model/src/Components/Register.jsx
+++ user_model/src/Components/Register.jsx
@@ -4,6 +4,41 @@ import '../JS/register.js'
 
 
 export default function Register() {
+
+
+    let [name, setName] = useState("")
+    let [email, setEmail] = useState("")
+    let [pswd, setPswd] = useState("")
+    let [gender, setGender] = useState("")
+    let [age, setAge] = useState(0)
+
+    function clear() {
+        setName("");
+        setEmail("");
+        setGender("");
+        setPswd("");
+        setAge(0);
+    }
+
+    async function register_user(e) {
+        e.preventDefault()
+        try {
+            let userapi = await axios.post("http://localhost:3001/gym/user", {
+                name: name,
+                email: email,
+                password: pswd,
+                gender: gender,
+                age: age
+            })
+            toast.success(userapi.data.msg)
+        } catch (error) {
+            console.log(error)
+            // toast.error(userapi.data.msg)
+        }
+    }
+
+
+
     return (
         <div>
             <div class="container registration">
@@ -11,11 +46,12 @@ export default function Register() {
                     <p>Registration</p>
                 </div>
 
-                <div className='form'>
+                <div className='form my-5 bg-light shadow rounded'>
                     <div class="user_details">
                         <div class="input_box">
                             <label for="name">Full Name</label>
-                            <input type="text" id="name" placeholder="Enter your name" required />
+                            <input type="text"value={name} onChange={(e)=>{setName(e.target.value)}}
+                            id="name" placeholder="Enter your name" required />
                         </div>
                         <div class="input_box">
                             <label for="username">Username</label>
@@ -39,21 +75,21 @@ export default function Register() {
                         </div>
                     </div>
                     <div class="gender">
-                        <span class="gender_title">Gender</span>
+                        <span class="gender_title mb-3">Gender</span>
                         <input type="radio" name="gender" id="radio_1" />
                         <input type="radio" name="gender" id="radio_2" />
                         <input type="radio" name="gender" id="radio_3" />
 
-                        <div class="category">
-                            <label for="radio_1">
+                        <div class="category d-flex flex-column justify-content-center">
+                            <label for="radio_1" className=''>
                                 <span class="dot one"></span>
                                 <span>Male</span>
                             </label>
-                            <label for="radio_2">
+                            <label for="radio_2" className=''>
                                 <span class="dot two"></span>
                                 <span>Female</span>
                             </label>
-                            <label for="radio_3">
+                            <label for="radio_3" className=''>
                                 <span class="dot three"></span>
                                 <span>Prefer not to say</span>
                             </label>
